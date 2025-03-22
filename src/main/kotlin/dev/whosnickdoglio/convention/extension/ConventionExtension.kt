3d1ff0acf1413b38289138469bba9e95b386c34d
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-03-22 13:28:03 -0400

    Start docs for ConventionExtension

--- src/main/kotlin/dev/whosnickdoglio/convention/extension/ConventionExtension.kt
+++ src/main/kotlin/dev/whosnickdoglio/convention/extension/ConventionExtension.kt
@@ -27,8 +27,19 @@ package dev.whosnickdoglio.convention.extension
 import javax.inject.Inject
 import org.gradle.api.Project
 
+/**
+ * A Gradle extension that provides configuration methods for common build conventions.
+ *
+ * This extension can be accessed via the `convention` DSL.
+ */
 public abstract class ConventionExtension @Inject constructor(private val project: Project) {
 
+    /**
+     * Enables code coverage reporting using the Kover plugin.
+     *
+     * This method applies the 'org.jetbrains.kotlinx.kover' plugin to the project, which provides
+     * code coverage analysis for Kotlin projects.
+     */
     public fun enableCodeCoverageWithKover() {
         project.pluginManager.apply("org.jetbrains.kotlinx.kover")
     }
