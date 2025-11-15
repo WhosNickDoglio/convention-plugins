e8062d91589afeafedea6c6c695f879f8402d038
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-11-15 12:23:44 -0500

    WIP Kover code coverage filtering

--- submodule-plugins/src/main/kotlin/dev/whosnickdoglio/convention/ConventionExtension.kt
+++ submodule-plugins/src/main/kotlin/dev/whosnickdoglio/convention/ConventionExtension.kt
@@ -3,22 +3,26 @@
 package dev.whosnickdoglio.convention
 
 import javax.inject.Inject
+import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.model.ObjectFactory
 
 /**
  * A Gradle extension that provides configuration methods for common build conventions.
  *
  * This extension can be accessed via the `convention` DSL.
  */
-public abstract class ConventionExtension @Inject constructor(private val project: Project) {
-
+public abstract class ConventionExtension
+@Inject
+constructor(private val objects: ObjectFactory, private val project: Project) {
     /**
      * Enables code coverage reporting using the Kover plugin.
      *
      * This method applies the 'org.jetbrains.kotlinx.kover' plugin to the project, which provides
      * code coverage analysis for Kotlin projects.
      */
-    public fun enableCodeCoverageWithKover() {
+    public fun codeCoverage(action: Action<KoverHandler>) {
         project.pluginManager.apply("org.jetbrains.kotlinx.kover")
+        action.execute(objects.newInstance(KoverHandler::class.java))
     }
 }
---
+++ submodule-plugins/src/main/kotlin/dev/whosnickdoglio/convention/KoverHandler.kt
@@ -0,0 +1,19 @@
+// Copyright (C) 2025 Nicholas Doglio
+// SPDX-License-Identifier: MIT
+
+package dev.whosnickdoglio.convention
+
+import javax.inject.Inject
+import kotlinx.kover.gradle.plugin.dsl.KoverProjectExtension
+import kotlinx.kover.gradle.plugin.dsl.KoverReportsConfig
+import org.gradle.api.Action
+import org.gradle.api.Project
+
+public abstract class KoverHandler @Inject constructor(project: Project) {
+
+    private val extension = project.extensions.getByType(KoverProjectExtension::class.java)
+
+    public fun reports(block: Action<KoverReportsConfig>) {
+        extension.reports(block)
+    }
+}
